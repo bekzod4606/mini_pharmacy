3b1555856d6573e1eac6d850fc3fe9287a8210b8
bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-04-11 17:24:58 +0500

    setting up swagger and adding requirements

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PharmacyViewSet, MedicineViewSet, OrderViewSet, OrderRetrieveUpdateDestroy
+from app.views import PharmacyViewSet, MedicineViewSet, OrderViewSet, OrderRetrieveUpdateDestroy
 
 urlpatterns = [
-    path('pharmacy/', PharmacyViewSet.as_view()),
-    path('medicine/', MedicineViewSet.as_view()),
-    path('orders/', OrderViewSet.as_view(), name='order_create_list'),
-    path('orders/<int:id>/', OrderRetrieveUpdateDestroy.as_view(), name='order_detail'),
+    path('pharmacies/', PharmacyViewSet.as_view(), name='pharmacies'),
+    path('medicines/', MedicineViewSet.as_view(), name='medicines'),
+    path('orders/', OrderViewSet.as_view(), name='orders'),
+    path('orders/<int:id>/', OrderRetrieveUpdateDestroy.as_view(), name='order-details'),
 ]
